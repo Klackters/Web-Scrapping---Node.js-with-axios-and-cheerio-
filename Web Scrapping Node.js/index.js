d3dc4fa3434a77e76c60ad9600e4ae0fed4808cb
brenofflank <117662374+brenofflank@users.noreply.github.com>
2022-11-09 15:24:37 -0300

    web scrapping wikipedia
    Web Scrapping da wikipedia, utilizando "axios" e "cheerio". Coleta os filmes vencedores do prêmio oscar de todos os anos desde 1929, informando nome e ano.

---
+++ Web Scrapping Node.js/index.js
@@ -0,0 +1,26 @@
+/* O objetivo desta aplicação é fazer um web scrapping no site da wikipedia, na seção que fala sobre os filmes vencedores do prêmio óscar desde 1929 */
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://pt.wikipedia.org/wiki/Oscar_de_melhor_filme';
+
+async function getMovies() {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    const list = [];
+    $(".wikitable tbody tr").each((i, elem) => {
+        const name = $(elem).find('td[style*="background:#FAEB86"]').last().text().replace("\n", "");
+        if(name !== ''){
+            const year = $(elem).find('td[style*="background:#FAEB86"]').first().prev('td').text().replace("\n", "").slice(-4);
+            movie = { name, year };
+            list.push(movie);
+        }
+    });
+
+    console.log(list);
+
+}
+
+getMovies();
